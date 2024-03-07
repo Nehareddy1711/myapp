ecd48f581d30736234a883fb4cae4952ad26a354
Nehareddy1711 <nehaatham@lyrostech.com>
2024-03-07 14:40:39 +0530

    yes

--- src/StateComponent.js
+++ src/StateComponent.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import {useState, useEffect} from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
-// export default function StateComponent() {
-//     const [count, setCount] = useState(0);
-//     console.log("Component loaded");
+export default function App6() {
+  const [runs, setRuns] = useState(0);
+  const [wickets, setWickets] = useState(0);
+  // console.log("Component loaded");
+  useEffect(() => {
+    if (wickets > 0) console.log(`Wickets:${wickets}.Better Luck Next Time!`);
+  }, [wickets]);
+
+  useEffect(() => {
+    if (runs > 0) console.log(`Good Job! Score:${runs}`);
+  }, [runs]);
+
+  return (
+    <>
+      <button onClick={() => setRuns((prevState) => prevState + 1)}>
+        Runs({runs})
+      </button>
+      <button onClick={() => setWickets((prevState) => prevState + 1)}>
+        Wickets({wickets})
+      </button>
+    </>
+  );
+}
+
+///////////////
+
+// export default function App6() {
+//   const [count, setCount] = useState(0);
+//   console.log("Component loaded");
 //   return (
-//     <div>
-//         <button onClick={() => setCount((prevState) => prevState + 1)}>Click</button>
-//         <span>{count}</span>
-//     </div>
+//     <>
+//       <button onClick={() => setCount((prevState) => prevState + 1)}>
+//         Click
+//       </button>
+//       <span>{count}</span>
+//     </>
 //   );
 // }
 
-export default function StateComponent() {
-  const [runs, setRuns] = useState(0);
-  const [wickets, setWickets] = useState(0);
-  console.log("Component loaded");
-  useEffect(() => {
-    console.log("Better Luck Next Time!");
-  },[wickets]);
-return (
-  <div>
-      <button onClick={() => setRuns((prevState) => prevState + 1)}>Runs({runs})</button>
-      <button onClick={() => setWickets((prevState) => prevState + 1)}>Wickets({wickets})</button>
-  </div>
-);
-}
+///////////////
+// export default function App6() {
+//   const [count, setCount] = useState(0);
+//   console.log("Component loaded");
+//   return (
+//     <>
+//       <button onClick={() => setCount((prevState) => 5)}>Click</button>
+//       <span>{count}</span>
+//     </>
+//   );
+// }
+///////////////
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import StateComponent from './StateComponent';
+import App9 from './App9';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +12,6 @@ root.render(
     {/* <App2 user=""/> */}
     {/* <App3 names={["Neha","Surendra"]}/> */}
     {/* <App3 students={{name:'Surendra',age:24}}/> */}
-    <StateComponent/>
+    <App9/>
   </React.StrictMode>
 );
