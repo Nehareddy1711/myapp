5b71e93c7f541c57c6bf7bcfc9a679426135b7a4
Nehareddy1711 <nehaatham@lyrostech.com>
2024-03-08 14:00:09 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App12 from './App12';
+import Loginform from './Loginform';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +12,6 @@ root.render(
     {/* <App2 user=""/> */}
     {/* <App3 names={["Neha","Surendra"]}/> */}
     {/* <App3 students={{name:'Surendra',age:24}}/> */}
-    <App12/>
+    <Loginform/>
   </React.StrictMode>
 );
