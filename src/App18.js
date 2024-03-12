1a22a27eda563a9273e4d4a2da470ba983cc798a
Nehareddy1711 <nehaatham@lyrostech.com>
2024-03-12 16:01:47 +0530

    commit

---
+++ src/App18.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import App18child from './App18child'
+export default function App19() {
+  return (
+    <div>
+        <App18child>
+            <h1>Hello World</h1>
+        </App18child>
+    </div>
+  )
+  }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App16 from './App16';
+import App17 from './App17';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +12,6 @@ root.render(
     {/* <App2 user=""/> */}
     {/* <App3 names={["Neha","Surendra"]}/> */}
     {/* <App3 students={{name:'Surendra',age:24}}/> */}
-    <App16/>
+    <App17/>
   </React.StrictMode>
 );
